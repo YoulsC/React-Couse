dfdce9bfb6c05ca94398acc913458235db69637f
YoulsC <al265852@edu.uaa.mx>
2025-08-21 23:30:16 -0600

    24s3

--- 01-reforzamiento/src/bases/07-array-destructuring.ts
+++ 01-reforzamiento/src/bases/07-array-destructuring.ts
@@ -13,14 +13,30 @@ const returnsArrayFn = () => {
 
 const [letters, numbers] = returnsArrayFn();
 
-console.log(letters, numbers);
+//console.log(letters, numbers);
 
-const useState = () => {
+//Tarea class22
+// const useState = (n1:string) => {
     
-const setName = (newName:String) => {
-    console.log(newName);
-    } 
+// const setName = (newName:String) => {
+//     console.log(newName);
+//     } 
 
-    return ['Goku',setName ] as const;
+//     return [n1,setName ] as const;
 
-}
+// }
+
+//sol del profe
+const useState = (n1:string) => {
+    return [
+        n1,
+        (newName:string) => {
+        console.log(newName);
+    }, 
+ ] as const;
+};
+
+const [name, setName] = useState('Goku');
+console.log(name);
+setName('Vegeta');
+//
--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -3,9 +3,12 @@ import './style.css';
 // import './bases/02-template-string';
 // import './bases/03-object-literal';
 // import './bases/04-arrays';
-//import './bases/05-functions'
+//import './bases/05-functions';
 //import './bases/06-obj-destructuring';
-import './bases/07-array-destructuring'
+//import './bases/07-array-destructuring';
+import './bases/08-imp-exp';
+
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
