68f4c46c2ce9b66e8475d2dce01e5e4c917128d6
YoulsC <al265852@edu.uaa.mx>
2025-08-30 03:37:55 +0200

    25S1

--- 01-reforzamiento/src/bases/08-imp-exp.ts
+++ 01-reforzamiento/src/bases/08-imp-exp.ts
@@ -1,6 +1,4 @@
-import { heroes, type Hero } from "../data/heroes.data"
-
-
+import { heroes, type Hero, Owner } from "../data/heroes.data"
 
 
 const getHeroById = (id:number):Hero | undefined => {
@@ -16,4 +14,19 @@ const hero = heroes.find( (hero) => {
  return hero;
 };
 
-console.log(getHeroById(1));
+//console.log(getHeroById(1));
+
+export const getHeroesByOwner = (owner:Owner):Hero[] | undefined => {
+    const heroesByOwner:(Hero[]| undefined) = heroes.filter((hero)=>{
+        return hero.owner===owner;
+    });
+    
+    return heroesByOwner;
+}
+
+// getHeroesByOwner => Hero []
+// Filtra heroes por su propietario
+// @param Owner - el propietario por el cual filtar (dc o Marvel), usar la enum Owner
+// @returns Array de heroes pertenecientes al propietario
+
+// console.log(getHeroesByOwner('Marvel') );
